0e6deb6d604bfdda44567ebf1ac4b30a51692551
Diki-xcidic <agungmustaqimdiki@gmail.com>
2023-07-03 09:59:48 +0700

    test mend scanning 2

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CI/CD Security pipeline is running!!!');
+  res.send('CI/CD Security pipeline is running!');
 });
 
 module.exports = app;
